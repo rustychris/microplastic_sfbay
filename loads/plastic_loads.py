70b1d348fb4eb5ce6ee75811a7e6e557edff82e0
Rusty Holleman <rustychris@gmail.com>
2019-08-16 12:13:12 -0700

    No longer adjust concentrations here -- do that in conc_func.

--- loads/plastic_loads.py
+++ loads/plastic_loads.py
@@ -227,17 +227,20 @@ for station in stationcodes:
 
     # adjust for the fraction of particles with w_s, and that these 8 POTWs
     # are 70% of the overall discharge
-    adj_bin_conc = (1.00/0.70) * bin_conc * total_count / w_s_count
-        
+    # 2019-08-16: no longer adjust for the 70% figure here.
+    #adj_bin_conc = (1.00/0.70) * bin_conc * total_count / w_s_count
+    adj_bin_conc = bin_conc * total_count / w_s_count
     ds.conc.sel(source=station).values[:] = adj_bin_conc
 
 # StationCode: 'CCCSD', 'EBDA', 'EBMUD', 'FSSD', 'LABQA', 'PA', 'SFPUC', 'SJ', 'SUNN'
 
 # ds depends on w_s_centers, so don't write it out
 
+ds.conc.attrs['units']='particle l-1'
+
 ##
 
-out_fn='plastic_loads-7classes.nc'
+out_fn='plastic_loads-7classes-v02.nc'
 os.path.exists(out_fn) and os.unlink(out_fn)
 ds.to_netcdf(out_fn)
 
@@ -247,19 +250,24 @@ ds.to_netcdf(out_fn)
 df_out=ds.to_dataframe().unstack()
 print(str(df_out))
 
-# ------------------- End point ---------------
-#                  conc                                                            
-#  w_s          -0.0500   -0.0050   -0.0005    0.0000    0.0005    0.0050    0.0500
-#  source                                                                          
-#  CCCSD       0.025905  0.049891  0.020148  0.000000  0.017270  0.009594  0.002878
-#  EBDA        0.009796  0.010496  0.011895  0.000000  0.013994  0.005598  0.002099
-#  EBMUD       0.021510  0.074817  0.018704  0.000000  0.019639  0.015899  0.001870
-#  FSSD        0.000000  0.002240  0.001493  0.000000  0.005226  0.003360  0.000747
-#  PA          0.000000  0.000930  0.001859  0.000000  0.004881  0.005810  0.000232
-#  SFPUC       0.037653  0.131087  0.050204  0.000000  0.029285  0.027891  0.001395
-#  SJ          0.005691  0.017478  0.004878  0.001219  0.004878  0.002845  0.000406
-#  SUNN        0.001323  0.002646  0.009259  0.000000  0.011905  0.014550  0.000000
-#  stormwater  0.250191  0.552146  0.707437  0.017255  1.147429  6.159880  0.552146
+df_out.to_excel(out_fn.replace('.nc','.xlsx'))
+
+# This is slightly out-dated -- see the .xlsx for a better
+# view.
+
+#OLD ------------------- End point ---------------
+#OLD                 conc                                                            
+#OLD w_s          -0.0500   -0.0050   -0.0005    0.0000    0.0005    0.0050    0.0500
+#OLD source                                                                          
+#OLD CCCSD       0.025709  0.049513  0.019043  0.000000  0.018091  0.009522  0.003809
+#OLD EBDA        0.009796  0.010496  0.011895  0.000000  0.013994  0.005598  0.002099
+#OLD EBMUD       0.021121  0.077138  0.016530  0.000000  0.020203  0.015611  0.001837
+#OLD FSSD        0.000000  0.002287  0.001307  0.000000  0.005226  0.003593  0.000653
+#OLD PA          0.000000  0.001600  0.001143  0.000000  0.005028  0.005714  0.000229
+#OLD SFPUC       0.038278  0.133972  0.038278  0.000000  0.035544  0.030075  0.001367
+#OLD SJ          0.005753  0.017670  0.003287  0.001233  0.005342  0.003698  0.000411
+#OLD SUNN        0.001167  0.005836  0.004669  0.000000  0.014006  0.014006  0.000000
+#OLD stormwater  0.243128  0.599137  0.642553  0.017366  1.172225  6.156353  0.555722
 
 # Note that these are concentrations, so not loads...
 # Interesting conclusion that on the buoyant end, the gap is not nearly so large,
