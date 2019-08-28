666bf4200c59f70ff6a5875d5eb54778f46abd3a
Rusty Holleman <rustychris@gmail.com>
2019-08-27 18:53:53 -0700

    Adjusted wastewater concentrations

--- loads/plastic_loads.py
+++ loads/plastic_loads.py
@@ -188,7 +188,6 @@ ds['conc_raw'].sel(source='stormwater').values[:]= bin_conc_raw
 ##
 
 # And wastewater:
-# 
 df=plastic_data.effluent_df
 
 # StationCode: 'CCCSD', 'EBDA', 'EBMUD', 'FSSD', 'LABQA', 'PA', 'SFPUC', 'SJ', 'SUNN'
@@ -249,20 +248,54 @@ for sample_id in df.SampleID.unique():
         print(matches)
         raise Exception("Matches are off")
 ##
-stationcodes=df.StationCode.unique()
+
+# Summarize the blank counts by category
+include_lab_blanks=True
+
+if include_lab_blanks:
+    blanks=df[ ~df['field_sample_p'] ].copy() # use field and lab blanks
+else:
+    blanks=df[ (df['StationCode']!='LABQA') & (~df['field_sample_p']) ].copy() # only use field blanks
+
+field=df[df['field_sample_p']].copy()
+    
+n_blank_samples=len(blanks['SampleID'].unique())
+n_field_samples=len(field['SampleID'].unique())
+
+print("--- Effluent Blanks ---")
+if include_lab_blanks:
+    print(f"  Considering Field & Lab Blanks, {n_blank_samples:3} distinct blank samples")
+else:
+    print(f"  Considering Field Blanks only, {n_blank_samples:3} distinct blank samples")
+    
+# 15
+print(f"                                 {n_field_samples:3} distinct field samples")
+
+# Assume that each sample accumulates the same counts of erroneous particles.
+blank_category_per_sample=blanks.groupby('Category_Final').size() / n_blank_samples
+print("Per-blank sample, per-category counts")
+print(blank_category_per_sample)
+print()
+
+##
+
+# Derate
+# All particles start with weight of 1.0
+# The derating has to be done per-station.
+field['derated']=1.0
+
+stationcodes=field.StationCode.unique()
 
 for station in stationcodes:
-    if station in ['LABQA']: # station ids we don't care about
-        continue
-    df1=df[ df.StationCode==station ]
-    total_count=len(df1)
-    df2=df1[ np.isfinite(df1.w_s) ]
-    bins=utils.nearest(w_s_centers,df2.w_s.values)
-    w_s_count=len(df2)
-    
+    field_station=field[(field.StationCode==station)].copy()
+    total_count=len(field_station)
+    field_valid=np.isfinite(field_station.w_s)
+    bins=utils.nearest(w_s_centers,field_station[field_valid].w_s.values)
+    w_s_count=field_valid.sum()
+
     # what was the total volume sampled for this station?
     sample_volume_l=0
-    sample_ids=df1.SampleID.unique()
+    sample_ids=field_station.SampleID.unique()
     for patt in sample_volume_patts.keys(): # does this volume go with this station?
         for samp_id in sample_ids: # yes, iff 1 or more sample_ids match
              if re.match(patt,samp_id) is not None:
@@ -270,53 +303,96 @@ for station in stationcodes:
                  break
     print(f"{station:20} total sample volume {sample_volume_l:7} l")
 
-    bin_conc=np.zeros(len(w_s_centers))
-    for bin_idx,particles in utils.enumerate_groups(bins):
-        bin_conc[bin_idx] = len(particles) / sample_volume_l
+    # Derate
+    n_field_samples=len(sample_ids)
+    
+    for cat,blank_freq in blank_category_per_sample.iteritems():
+        print(f"Effluent, category={cat}.  Per-blank count {blank_freq:.3f}")
+        cat_sel=field_station['Category_Final']==cat    
+        n_field_per_cat=cat_sel.sum() # total number of field samples at this station of this category
+        print(f"    At {station}, field count over {n_field_samples:3} samples: {n_field_per_cat}, {n_field_per_cat/n_field_samples:.2f} per sample")
+        if n_field_per_cat>0:
+            real_derate=(n_field_per_cat - n_field_samples*blank_freq) / n_field_per_cat
+        else:
+            real_derate=0.0
+        derate=max(0.0,real_derate)
+        print(f"    de-rating: {derate:.3f} ({real_derate:.3f})")
+        print()
+        field_station.loc[ cat_sel, 'derated']=derate
 
-    # adjust for the fraction of particles with w_s, and that these 8 POTWs
-    # are 70% of the overall discharge
-    # 2019-08-16: no longer adjust for the 70% figure here.
-    #adj_bin_conc = (1.00/0.70) * bin_conc * total_count / w_s_count
-    adj_bin_conc = bin_conc * total_count / w_s_count
-    ds.conc.sel(source=station).values[:] = adj_bin_conc
+    bin_conc_raw=np.zeros(len(w_s_centers))
+    bin_conc=np.zeros(len(w_s_centers))
+
+    # Adjustment factor for how many samples have w_s vs total counted
+    adjust=total_count / w_s_count
+    for bin_idx,particles in utils.enumerate_groups(bins):
+        bin_conc_raw[bin_idx] = len(particles) * adjust/sample_volume_l
+        bin_conc[bin_idx]=field_station['derated'].values[field_valid][particles].sum() * adjust/sample_volume_l 
+
+    ds.conc_raw.sel(source=station).values[:] = bin_conc_raw
+    
 
 # StationCode: 'CCCSD', 'EBDA', 'EBMUD', 'FSSD', 'LABQA', 'PA', 'SFPUC', 'SJ', 'SUNN'
 
-# ds depends on w_s_centers, so don't write it out
-
 ds.conc.attrs['units']='particle l-1'
-
-##
-
-out_fn='plastic_loads-7classes-v02.nc'
-os.path.exists(out_fn) and os.unlink(out_fn)
-ds.to_netcdf(out_fn)
-
-##
 
 # Print a nice table:
 df_out=ds.to_dataframe().unstack()
 print(str(df_out))
 
+##
+
+out_fn='plastic_loads-7classes-v03.nc'
+os.path.exists(out_fn) and os.unlink(out_fn)
+ds.to_netcdf(out_fn)
+
+##
+
 df_out.to_excel(out_fn.replace('.nc','.xlsx'))
 
-# This is slightly out-dated -- see the .xlsx for a better
-# view.
+##
 
-#OLD ------------------- End point ---------------
-#OLD                 conc                                                            
-#OLD w_s          -0.0500   -0.0050   -0.0005    0.0000    0.0005    0.0050    0.0500
-#OLD source                                                                          
-#OLD CCCSD       0.025709  0.049513  0.019043  0.000000  0.018091  0.009522  0.003809
-#OLD EBDA        0.009796  0.010496  0.011895  0.000000  0.013994  0.005598  0.002099
-#OLD EBMUD       0.021121  0.077138  0.016530  0.000000  0.020203  0.015611  0.001837
-#OLD FSSD        0.000000  0.002287  0.001307  0.000000  0.005226  0.003593  0.000653
-#OLD PA          0.000000  0.001600  0.001143  0.000000  0.005028  0.005714  0.000229
-#OLD SFPUC       0.038278  0.133972  0.038278  0.000000  0.035544  0.030075  0.001367
-#OLD SJ          0.005753  0.017670  0.003287  0.001233  0.005342  0.003698  0.000411
-#OLD SUNN        0.001167  0.005836  0.004669  0.000000  0.014006  0.014006  0.000000
-#OLD stormwater  0.243128  0.599137  0.642553  0.017366  1.172225  6.156353  0.555722
+
+#                  conc                                                    \
+#  w_s          -0.0500   -0.0050   -0.0005    0.0000    0.0005    0.0050   
+#  source                                                                   
+#  CCCSD       0.017457  0.034913  0.011870  0.000000  0.013965  0.006983   
+#  EBDA        0.006716  0.007233  0.007233  0.000000  0.010333  0.004650   
+#  EBMUD       0.015244  0.052359  0.011267  0.000000  0.014581  0.011930   
+#  FSSD        0.000000  0.001407  0.000469  0.000000  0.003752  0.003049   
+#  PA          0.000000  0.000698  0.000349  0.000000  0.003840  0.004538   
+#  SFPUC       0.027176  0.090588  0.026170  0.000000  0.026170  0.023150   
+#  SJ          0.004212  0.011433  0.002407  0.000903  0.003911  0.003009   
+#  SUNN        0.000992  0.000992  0.001984  0.000000  0.011905  0.011905   
+#  stormwater  0.245147  0.513660  0.543446  0.016483  1.096585  6.195458   
+#  
+#                        conc_raw                                          \
+#  w_s           0.0500   -0.0500   -0.0050   -0.0005    0.0000    0.0005   
+#  source                                                                   
+#  CCCSD       0.002793  0.017457  0.034913  0.011870  0.000000  0.013965   
+#  EBDA        0.001550  0.006716  0.007233  0.007233  0.000000  0.010333   
+#  EBMUD       0.001326  0.015244  0.052359  0.011267  0.000000  0.014581   
+#  FSSD        0.000469  0.000000  0.001407  0.000469  0.000000  0.003752   
+#  PA          0.000175  0.000000  0.000698  0.000349  0.000000  0.003840   
+#  SFPUC       0.001007  0.028026  0.093421  0.026988  0.000000  0.022836   
+#  SJ          0.000301  0.004212  0.011433  0.002407  0.000903  0.003911   
+#  SUNN        0.000000  0.000992  0.000992  0.001984  0.000000  0.011905   
+#  stormwater  0.567930  0.245169  0.516607  0.569143  0.017512  1.217091   
+#  
+#                                  
+#  w_s           0.0050    0.0500  
+#  source                          
+#  CCCSD       0.006983  0.002793  
+#  EBDA        0.004650  0.001550  
+#  EBMUD       0.011930  0.001326  
+#  FSSD        0.003049  0.000469  
+#  PA          0.004538  0.000175  
+#  SFPUC       0.014532  0.001038  
+#  SJ          0.003009  0.000301  
+#  SUNN        0.011905  0.000000  
+#  stormwater  6.251819  0.569143  
+
+
 
 # Note that these are concentrations, so not loads...
 # Interesting conclusion that on the buoyant end, the gap is not nearly so large,
